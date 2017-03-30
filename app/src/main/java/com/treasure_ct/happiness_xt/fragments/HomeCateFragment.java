54b257376678c8f6f71e51453e7471fd9a9d69a9
treasure_ct <m18753377169@gmail.com>
2017-03-30 22:51:29 +0800

    home界面配置基本完成

---
+++ app/src/main/java/com/treasure_ct/happiness_xt/fragments/HomeCateFragment.java
@@ -0,0 +1,31 @@
+package com.treasure_ct.happiness_xt.fragments;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.treasure_ct.happiness_xt.R;
+
+public class HomeCateFragment extends BaseFragment {
+    //标志位，标志已经初始化完成
+    private boolean isPrepared;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home_cate, container, false);
+        //初始化view各控件
+        isPrepared = true;
+        lazyLoad();
+        return view;
+    }
+
+    @Override
+    protected void lazyLoad() {
+        if (!isPrepared || !isVisible)
+            return;
+        //填充数据
+    }
+}
--- app/src/main/java/com/treasure_ct/happiness_xt/fragments/HomeFragment.java
+++ app/src/main/java/com/treasure_ct/happiness_xt/fragments/HomeFragment.java
@@ -2,19 +2,74 @@ package com.treasure_ct.happiness_xt.fragments;
 
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.treasure_ct.happiness_xt.R;
+import com.treasure_ct.happiness_xt.adapter.HomeFragmentPagerAdapter;
 
-public class HomeFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
 
+public class HomeFragment extends Fragment implements ViewPager.OnPageChangeListener {
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        initFindId(view);
+        initTabLayout();
+        initViewPager();
+        viewPager.setCurrentItem(0);
+        viewPager.addOnPageChangeListener(this);
+        //联动
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        return view;
+    }
+    private void initFindId(View view) {
+        tabLayout = (TabLayout) view.findViewById(R.id.home_tabLayout);
+        viewPager = (ViewPager) view.findViewById(R.id.home_viewPager);
     }
 
+    private void initTabLayout() {
+        tabLayout.addTab(tabLayout.newTab().setText("新闻"));
+        tabLayout.addTab(tabLayout.newTab().setText("段子"));
+        tabLayout.addTab(tabLayout.newTab().setText("视频"));
+        tabLayout.addTab(tabLayout.newTab().setText("美食"));
+        tabLayout.addTab(tabLayout.newTab().setText("音乐"));
+        tabLayout.addTab(tabLayout.newTab().setText("新闻"));
+        tabLayout.addTab(tabLayout.newTab().setText("新闻"));
+        tabLayout.addTab(tabLayout.newTab().setText("新闻"));
+    }
+
+    private void initViewPager() {
+        List<Fragment> list = new ArrayList<>();
+        list.add(new HomeNewsFragment());
+        list.add(new HomeJokerFragment());
+        list.add(new HomeVideoFragment());
+        list.add(new HomeCateFragment());
+        list.add(new HomeMusicFragment());
+        HomeFragmentPagerAdapter pagerAdapter = new HomeFragmentPagerAdapter(getChildFragmentManager(), list);
+        viewPager.setAdapter(pagerAdapter);
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        viewPager.setCurrentItem(position);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
 }
